a09c04a22ab40a135f100a8c5198d1dd9433a676
richmond97 <Yeboah.riichmond97@gmail.com>
2020-01-28 23:06:30 +0000

    Initial Commit

---
+++ Prototype2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Prototype2
+//
+//  Created by Richmond Yeboah on 28/01/2020.
+//  Copyright © 2020 Richmond Yeboah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
