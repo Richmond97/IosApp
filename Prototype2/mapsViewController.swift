4c4e734f8b734b38c7723fd11612527ec08d34c5
richmond97 <Yeboah.riichmond97@gmail.com>
2020-03-26 01:24:42 +0000

    yolo added

---
+++ Prototype2/mapsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  mapsViewController.swift
+//  Prototype2
+//
+//  Created by Richmond Yeboah on 31/01/2020.
+//  Copyright © 2020 Richmond Yeboah. All rights reserved.
+//
+
+import UIKit
+
+class mapsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
